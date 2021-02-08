Fix incorrect q calculation

train.py
@@ -38,15 +38,12 @@ if __name__ == "__main__":
     total = torch.arange(args.number_points)
     print(f"Training with early exaggeration for {stop_lying_iter} epochs")
     for epoch in range(args.epochs):
-        # print(f"Epoch {epoch}")
+        print(f"Epoch {epoch}")
         model.zero_grad()
-        total_loss = 0
         loss = model(p)
         loss.backward()
 
         opt.step()
-
-        total_loss += loss.item()
 
         if epoch == mom_switch_iter:
             print("Momentum switch")

tsne_module.py
@@ -14,7 +14,7 @@ class tSNE(nn.Module):
         all_y = self.embedding.weight
         dist = F.pdist(all_y).pow(2)
         dist = (1. + dist).pow(-1.0 * self.degrees_of_freedom)
-        q = torch.clamp(dist / dist.sum(), min=eps)
+        q = torch.clamp(dist / (2 * dist.sum()), min=eps)
 
         # log_loss = pij * (torch.log(dist) - torch.log(qij))
         log_loss = p.dot(torch.log(torch.clamp(p, min=eps)) - torch.log(q))